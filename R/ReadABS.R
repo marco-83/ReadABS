7797182cffe9176a36050667acf40250c1d801e7
marco-83 <marco.hatt@hotmail.com>
2020-05-15 09:23:48 +1000

    Initial commit

--- R/ReadABS.R
+++ R/ReadABS.R
@@ -50,7 +50,7 @@
 
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname)
-  #xlsxwriter <<- reticulate::import("xlsxwriter", delay_load = TRUE)
+  pd <<- reticulate::import("pandas", delay_load = TRUE)
 
   # ABStable <- reticulate::import_from_path(
   #   module = "ABStable",
